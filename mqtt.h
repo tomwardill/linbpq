4e73030cfbe296948f4888369a7b642fe94b876b
Tom Wardill <tom@howrandom.net>
2024-10-06 16:30:04 +0100

    Add Mail events

--- mqtt.h
+++ mqtt.h
@@ -5,3 +5,6 @@ void MQTTKISSTX(void *message);
 void MQTTKISSTX_RAW(char* buffer, int bufferLength, void* PORT);
 void MQTTKISSRX(void *message);
 void MQTTKISSRX_RAW(char* buffer, int bufferLength, void* PORT);
+
+
+void MQTTMessageEvent(void *message);
